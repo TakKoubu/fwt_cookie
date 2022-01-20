c363a2e30a41a1edafd386e5c389445374344b35
TakKokubu <kokubu.tk@gmail.com>
2022-01-20 15:34:49 +0900

    headerの追加

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,9 +1,5 @@
 <template>
   <section class="post-list">
-    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
-    <TheSidenav
-      :show="displaySidenav"
-      @close="displaySidenav = false" />
     <PostPreview
       v-for="post in posts"
       :key="post.id"
